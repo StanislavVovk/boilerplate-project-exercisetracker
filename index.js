020270dfaa6cc54bb885a17619022f48d9e65dff
Stas Siuris <stassiuris@gmail.com>
2023-07-05 12:36:48 +0300

    fix

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post('/api/users/:_id/exercises', (req, res) => {
     const user = userData.find(user => user._id === id)
     user.count++
     user.log.push({description, duration, date})
-    res.json({"_id": id, username, description, duration, date})
+    res.json({"_id": id, username, date, duration, description})
 })
 
 app.get('/api/users/:_id/logs', (req, res) => {
